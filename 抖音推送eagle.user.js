cbe4e29a2e6e59b9ab1b2b0eaf54b3f411501484
JDK <39272014+jiebukai@users.noreply.github.com>
2026-09-19 16:48:05 +0800

    Update 抖音推送eagle.user.js

--- 抖音推送eagle.user.js
+++ 抖音推送eagle.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name            抖音推送eagle
+// @name            抖音图集/视频推送eagle
 // @namespace       https://github.com/jiebukai/tampermonkey
 // @version         1.1
 // @description     把抖音作品（视频/图集）推送到 Eagle 素材库，可选目标文件夹与标签；保留上游的下载能力
